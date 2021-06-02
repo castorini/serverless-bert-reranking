7ceeff7879f9663b814dfbd43e2fb2c28ad89cb4
Jiarui Zhang <jrzhang9912@gmail.com>
2021-06-02 00:33:54 -0400

    updated upload script

--- upload_model.py
+++ upload_model.py
@@ -17,8 +17,9 @@ try:
         s3_client.create_bucket(Bucket=bucket_name,
                                 CreateBucketConfiguration=location)
 
-    s3_client.upload_file('model.bin', 'model', 'model/model.bin')
-    s3_client.upload_file('model.bin', 'model', 'model/config.json')
-    s3_client.upload_file('model.bin', 'model', 'model/vocab.txt')
+    # reference https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
+    s3_client.upload_file('model/model.bin', 'model', 'model')
+    s3_client.upload_file('model/config.json', 'model', 'config')
+    s3_client.upload_file('model/vocab.txt', 'model', 'vocab')
 except ClientError as e:
     logging.error(e)
