584de48e5bfe87836c1b4813145f1b12d37c1e2a
Mayank Anand <m7anand@uwaterloo.ca>
2021-06-03 23:46:54 +0530

    Created first stage of e2e testing

---
+++ run_end_to_end.py
@@ -0,0 +1,23 @@
+import csv
+import requests
+import json
+
+# Reading the evaluation dataset
+tsv_file = open("queries.eval.small.tsv")
+read_tsv = csv.reader(tsv_file, delimiter="\t")
+
+api_url = "https://7wl1vh4ftb.execute-api.us-east-2.amazonaws.com/Prod/search/"
+
+count = 1
+
+# Here we extract the query string from the row and pass it on to our API
+# which returns a json object containing the relevant docids
+
+for row in read_tsv:
+    params_args = {
+        "query": row[1],
+        "max_docs": 1000
+    }
+    r = requests.get(url=api_url, params=params_args)
+    print(count, r.json())
+    count += 1
