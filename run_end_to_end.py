56b9a321bfd9ec6e06d401f573724831d1450e1d
Jiarui Zhang <jrzhang9912@gmail.com>
2021-06-09 23:11:11 -0400

    update e2e script

--- run_end_to_end.py
+++ run_end_to_end.py
@@ -3,6 +3,7 @@ import requests
 import json
 import time
 import fetch_msmarco_passage_all
+from concurrent.futures import ThreadPoolExecutor
 
 # Reading the evaluation dataset
 tsv_file = open("queries.eval.small.tsv")
@@ -16,22 +17,52 @@ count = 1
 # Here we extract the query string from the row and pass it on to our API
 # which returns a json object containing the relevant docids
 
-# top k result
-k = 3
+# top k results
+k = 100
+size = 10
+docs = []
+limit = 100
+
+def rerank(i):
+    rerank_params = {"docs": docs[i * size : i * size + size], "query": row[1]}
+    while True:
+        r = requests.post(rerank_url, json=rerank_params)
+        if not r:
+            print("retry rerank", i)
+        else:
+            break
+    # return i
+    return r.json()
 
 for row in read_tsv:
-    time.sleep(0.5)
     params_args = {
         "query": row[1],
         "max_docs": 1000
     }
-    r = requests.get(url=search_url, params=params_args).json()
+
+    # try until it works
+    while True:
+        r = requests.get(url=search_url, params=params_args)
+        if not r:
+            print("retry search")
+        else:
+            break
+
+    r = r.json()
     docs = fetch_msmarco_passage_all.get_documents(r)
-    rerank_params = {"docs": docs, "query": row[1]}
-    # TODO: split to 100 req here
-    # 1000/100 will timeout
-    r = requests.post(rerank_url, json=rerank_params).json()
-    print(sorted(r, key=lambda x: x[1], reverse=True)[0:k])
-    
-    break
+    partitions = len(docs) // size
+    with ThreadPoolExecutor(max_workers=partitions) as pool:
+        a = list(pool.map(rerank, range(0, partitions)))
+
+    flat = [item for sublist in a for item in sublist]
+
+    result = sorted(flat, key=lambda x: x[1], reverse=True)[0:k]
+    with open('output.txt', 'w') as f:
+        for idx, item in enumerate(result):
+            rank = idx + 1
+            line =  row[0] + " " + "Q0 " + item[0] + " " + str(rank) + " -" + str(rank) + " TEAM\n"
+            f.write(line)
+    print(str(count) + ' / ' + str(limit) + ' done')
+    if count == limit:
+        break
     count += 1
